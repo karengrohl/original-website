38066bd3ba0ad84e3a7d4ebe1ed82aaa5ebb4dcc
ngen277 <ngen277@gmail.com>
2017-08-15 22:49:11 +0300

    angular js and angularjs ui router

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,3 +1,32 @@
-(function () {
+var application = angular.module(
+    "original-store",
+    ["ui.router"]
+);
 
-})();
+application.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("");
+    var root = {
+        abstract: true,
+        url: "",
+        views: {
+            "header@": {
+                templateUrl: "templates/header.html"
+            },
+            "footer@": {
+                templateUrl: "templates/footer.html"
+            }
+        }
+    };
+
+    var mainPage = {
+        url: "",
+        views: {
+            "main@": {
+                templateUrl: "templates/main.html"
+            }
+        }
+    };
+
+    $stateProvider.state("root", root);
+    $stateProvider.state("root.main", mainPage);
+});
