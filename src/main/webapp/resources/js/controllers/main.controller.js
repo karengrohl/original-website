c75794fbdf12e33116858dd76038b8991528e8c4
ngen277 <ngen277@gmail.com>
2017-08-23 21:54:33 +0300

    simple carousel of items on main page

--- src/main/webapp/resources/js/controllers/main.controller.js
+++ src/main/webapp/resources/js/controllers/main.controller.js
@@ -8,14 +8,7 @@
         mc.carousel = {
             slides: [
                 {id: 0},
-                {id: 1},
-                {id: 2},
-                {
-                    id: 3
-                },
-                {
-                    id: 4
-                }
+                {id: 1}
             ],
             interval: 5000
         };
