8acecdd530a45cdb357fe9a611407713cf46d7a0
ngen277 <ngen277@gmail.com>
2017-09-02 19:21:06 +0300

    login placeholder

--- src/main/webapp/resources/js/controllers/header.controller.js
+++ src/main/webapp/resources/js/controllers/header.controller.js
@@ -1,7 +1,23 @@
 (function () {
-    function headerController($rootScope) {
-        $rootScope.cartCount = 0.0;
-    }
+
 
     angular.module("original-store").controller("header.controller", headerController);
+
+    function headerController($rootScope, $uibModal) {
+
+        var ctrl = this;
+
+        $rootScope.cartCount = 0.0;
+
+        ctrl.openLoginDialog = function () {
+            $uibModal.open({
+                animation: true,
+                templateUrl: "templates/login.html",
+                controller: "main.controller",
+                controllerAs: "mc",
+                resolve: {}
+            });
+        }
+
+    }
 })();
