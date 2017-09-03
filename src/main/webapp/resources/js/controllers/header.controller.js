fe66566d644b761622da1ee7ac7363f7f17e1827
ngen277 <ngen277@gmail.com>
2017-09-03 23:21:42 +0300

    fix for navbar to collapse properly

--- src/main/webapp/resources/js/controllers/header.controller.js
+++ src/main/webapp/resources/js/controllers/header.controller.js
@@ -3,9 +3,24 @@
 
     angular.module("original-store").controller("header.controller", headerController);
 
-    function headerController($rootScope, $uibModal) {
+    function headerController($rootScope, $uibModal, $state) {
 
         var ctrl = this;
+
+        ctrl.navbar = {
+            isCollapsed: true,
+            collapse: function () {
+                if (!this.isCollapsed) {
+                    this.isCollapsed = !this.isCollapsed;
+                }
+            }
+        };
+
+        ctrl.changeState = function (state, params) {
+            ctrl.navbar.collapse();
+            $state.go(state, params);
+        };
+
 
         $rootScope.cartCount = 0.0;
 
