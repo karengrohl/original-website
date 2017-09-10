ac5457dcef2169d2210b104fc9735e52053ca35d
ngen277 <ngen277@gmail.com>
2017-09-11 00:13:33 +0300

    price and size filters for catalog

--- src/main/webapp/resources/js/directives/catalog.directive.js
+++ src/main/webapp/resources/js/directives/catalog.directive.js
@@ -6,12 +6,31 @@
             restrict: "E",
             templateUrl: "templates/elements/catalog.html",
             controller: controller,
-            controllerAs: "mc"
+            controllerAs: "cc",
+            scope: {}
         };
     }
 
     function controller() {
         var ctrl = this;
+
+        ctrl.filters = {
+            price: {
+                popover: {
+                    templateUrl: "templates/elements/catalog/filters/price.html"
+                }
+            },
+            size: {
+                popover: {
+                    templateUrl: "templates/elements/catalog/filters/size.html"
+                }
+            },
+            brand: {
+                popover: {
+                    templateUrl: "templates/elements/catalog/filters/brand.html"
+                }
+            }
+        }
 
     }
 })();
